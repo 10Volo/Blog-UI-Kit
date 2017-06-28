e87cc0631379f0ce49c825081c6c89b190f10c81
Kostyshyn <kostyshyn@ua.fm>
2017-06-28 15:02:44 +0300

    card styling3

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,22 @@
 var optionButton = document.querySelectorAll('.card-options');
 var dropdowns = document.querySelectorAll('.card-dropdown');
 
+
+function closeDropdowns(){
+	var dropdownsArray = Array.from(dropdowns);
+	for ( var i = 0; i < dropdownsArray.length; i++ ){
+		var d = dropdownsArray[i];
+		if (d.classList.contains('show')){
+			console.log('for removing', d);
+			d.classList.remove('show');
+		}
+	}
+};
+
 optionButton.forEach(function(el){
 	el.addEventListener('click', function(ev){
+		// closeDropdowns();
 		var dropdown = el.nextElementSibling;
-		// console.log(dropdowns);
-		dropdowns.forEach(function(d){
-			if (d.classList.contains('show')){
-				d.classList.remove('show');
-			}
-		});
 		dropdown.classList.toggle('show');
 	});
 });
